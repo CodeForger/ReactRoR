f48b7d68439c1d7bfe5b4ac4d82afbab180c6533
Zhang Ying <zhangying87@gmail.com>
2016-11-15 17:08:50 +0800

    integrate with basic flux to add comment to list

--- app/assets/javascripts/components/comment_list.js.jsx
+++ app/assets/javascripts/components/comment_list.js.jsx
@@ -1,11 +1,20 @@
 var CommentList = React.createClass({
+  componentDidMount: function() {
+    Store.addChangeListener(this._onChange);
+  },
+  componentWillUnmount: function() {
+    Store.removeChangeListener(this._onChange);
+  },
   render: function() {
     return (
       <div>
-        {JSON.parse(this.props.comments).map(function(comment, idx) {
+        {Store.comments().map(function(comment, idx) {
           return <Comment key={idx} {... comment} />;
         })}
       </div>
     );
+  },
+  _onChange: function() {
+    this.forceUpdate();
   }
 });
